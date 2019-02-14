25e4ee2047bc30df95fa2bbe801fae4c66982d54
Justin Han <gksksla7140@gmail.com>
2019-02-13 17:39:46 -0800

    build

--- server.js
+++ server.js
@@ -23,6 +23,14 @@ app.use(cookieParser())
   .use('/', routes);
 
 
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("../frontend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "..", "frontend", "build", "index.html"));
+  });
+}
+
 
 console.log(` >>>>>>>>>>Listening on port :  ${port} <<<<<<<<<<<<<<<<<`);
 app.listen(port)
